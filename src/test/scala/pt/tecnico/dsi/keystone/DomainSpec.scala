63ea9f5906ffce452902c81e485026023d484bfa
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2020-03-31 15:21:31 +0100

    Fix domain spec.

--- src/test/scala/pt/tecnico/dsi/keystone/DomainSpec.scala
+++ src/test/scala/pt/tecnico/dsi/keystone/DomainSpec.scala
@@ -6,7 +6,24 @@ import pt.tecnico.dsi.keystone.models.Domain
 class DomainSpec extends CRUDSpec[Domain]("domain", _.domains) {
   def stub = IO.pure(Domain(
     name = "domain-test",
-    enabled = true,
+    enabled = false,
     description = "Domain description",
   ))
+
+  def enabledStub = IO.pure(Domain(
+    name = "enabled-domain",
+    enabled = true,
+    description = "A domain that is enabled",
+  ))
+
+  s"The ${name} service" should {
+    s"forcefully delete an enabled ${name}" in {
+      for {
+        client <- scopedClient
+        expected <- enabledStub
+        obj <- client.domains.create(expected)
+        result <- client.domains.delete(obj.id, force = true).valueShouldIdempotentlyBe(())
+      } yield result
+    }
+  }
 }
