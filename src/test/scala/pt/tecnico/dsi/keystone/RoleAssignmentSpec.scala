91a5059d4e53be3dbc3c66bffac343ff5b906ee0
Afonso Matos <afonsolfmatos@gmail.com>
2020-04-06 19:37:19 +0100

    role assignment spec

--- src/test/scala/pt/tecnico/dsi/keystone/RoleAssignmentSpec.scala
+++ src/test/scala/pt/tecnico/dsi/keystone/RoleAssignmentSpec.scala
@@ -2,12 +2,64 @@ package pt.tecnico.dsi.keystone
 
 import cats.effect.IO
 import pt.tecnico.dsi.keystone.models.{Group, Role, User}
-import pt.tecnico.dsi.keystone.services.RoleAssignment
+import pt.tecnico.dsi.keystone.services.{CRUDService, GenericRoleAssignmentService, RoleAssignment}
 
 trait RoleAssignmentSpec[F] {
   this: CRUDSpec[F] =>
 
   def roleService: KeystoneClient[IO] => RoleAssignment[IO]
+  def test[T](stubIO: IO[T], crud: CRUDService[IO, T], roleAssignmentService: GenericRoleAssignmentService[IO]): Unit = {
+      s"list roles for a ${crud.name} in a ${name}" in {
+        for {
+          client <- scopedClient
+          obj <- stub
+          stub <- stubIO
+          objWithId <- service(client).create(obj)
+          stubWithId <- crud.create(stub)
+          isIdempotent <- roleAssignmentService.list(objWithId.id, stubWithId.id).compile.toList.map(_.length).valueShouldIdempotentlyBe(0)
+        } yield isIdempotent
+      }
+
+      s"check ${crud.name} role assignment" in {
+        for {
+          client <- scopedClient
+          obj <- stub
+          role <- roleStub
+          stub <- stubIO
+          roleWithId <- client.roles.create(role)
+          objWithId <- service(client).create(obj)
+          stubWithId <- crud.create(stub)
+          isIdempotent <- roleAssignmentService.check(objWithId.id, stubWithId.id, roleWithId.id).valueShouldIdempotentlyBe(false)
+        } yield isIdempotent
+      }
+
+      s"assign role to a ${crud.name}" in {
+        for {
+          client <- scopedClient
+          obj <- stub
+          role <- roleStub
+          stub <- stubIO
+          roleWithId <- client.roles.create(role)
+          objWithId <- service(client).create(obj)
+          stubWithId <- crud.create(stub)
+          isIdempotent <- roleAssignmentService.assign(objWithId.id, stubWithId.id, roleWithId.id).valueShouldIdempotentlyBe(())
+        } yield isIdempotent
+      }
+
+      s"delete ${crud.name} role assignment" in {
+        for {
+          client <- scopedClient
+          obj <- stub
+          role <- roleStub
+          stub <- stubIO
+          roleWithId <- client.roles.create(role)
+          objWithId <- service(client).create(obj)
+          stubWithId <- crud.create(stub)
+          isIdempotent <- roleService(client).roles.groups.delete(objWithId.id, stubWithId.id, roleWithId.id).valueShouldIdempotentlyBe(())
+        } yield isIdempotent
+      }
+
+  }
 
   def groupStub = IO.pure(Group(
     name = "test-group",
@@ -26,108 +78,11 @@ trait RoleAssignmentSpec[F] {
     domainId = None, // We cannot use Some(domainId) because listing roles by default does not list roles from all domains
   ))
 
-  s"The ${name} service" should {
-
-    s"list roles for a group in a ${name}" in {
-      for {
-        client <- scopedClient
-        obj <- stub
-        group <- groupStub
-        objWithId <- service(client).create(obj)
-        groupWithId <- client.groups.create(group)
-        roles <- roleService(client).roles.groups.list(objWithId.id, groupWithId.id).compile.toList
-      } yield true shouldBe true
-    }
-
-    s"check group role assignment" in {
-      for {
-        client <- scopedClient
-        obj <- stub
-        role <- roleStub
-        group <- groupStub
-        roleWithId <- client.roles.create(role)
-        objWithId <- service(client).create(obj)
-        groupWithId <- client.groups.create(group)
-        isIdempotent <- roleService(client).roles.groups.check(objWithId.id, groupWithId.id, roleWithId.id).valueShouldIdempotentlyBe(false)
-      } yield isIdempotent
-    }
-
-    s"assign role to a group" in {
-      for {
-        client <- scopedClient
-        obj <- stub
-        role <- roleStub
-        group <- groupStub
-        roleWithId <- client.roles.create(role)
-        objWithId <- service(client).create(obj)
-        groupWithId <- client.groups.create(group)
-        isIdempotent <- roleService(client).roles.groups.assign(objWithId.id, groupWithId.id, roleWithId.id).valueShouldIdempotentlyBe(())
-      } yield isIdempotent
-    }
-
-    s"delete group role assignment" in {
-      for {
-        client <- scopedClient
-        obj <- stub
-        role <- roleStub
-        group <- groupStub
-        roleWithId <- client.roles.create(role)
-        objWithId <- service(client).create(obj)
-        groupWithId <- client.groups.create(group)
-        isIdempotent <- roleService(client).roles.groups.delete(objWithId.id, groupWithId.id, roleWithId.id).valueShouldIdempotentlyBe(())
-      } yield isIdempotent
-    }
-
-    //#region users
-      s"list roles for a user in a ${name}" in {
-        for {
-          client <- scopedClient
-          obj <- stub
-          user <- userStub
-          objWithId <- service(client).create(obj)
-          userWithId <- client.users.create(user)
-          roles <- roleService(client).roles.users.list(objWithId.id, userWithId.id).compile.toList
-        } yield true shouldBe true
-      }
-
-      s"check user role assignment" in {
-        for {
-          client <- scopedClient
-          obj <- stub
-          role <- roleStub
-          user <- userStub
-          roleWithId <- client.roles.create(role)
-          objWithId <- service(client).create(obj)
-          userWithId <- client.users.create(user)
-          isIdempotent <- roleService(client).roles.users.check(objWithId.id, userWithId.id, roleWithId.id).valueShouldIdempotentlyBe(false)
-        } yield isIdempotent
-      }
-
-      s"assign role to a user" in {
-        for {
-          client <- scopedClient
-          obj <- stub
-          role <- roleStub
-          user <- userStub
-          roleWithId <- client.roles.create(role)
-          objWithId <- service(client).create(obj)
-          userWithId <- client.users.create(user)
-          isIdempotent <- roleService(client).roles.users.assign(objWithId.id, userWithId.id, roleWithId.id).valueShouldIdempotentlyBe(())
-        } yield isIdempotent
-      }
-
-      s"delete user role assignment" in {
-        for {
-          client <- scopedClient
-          obj <- stub
-          role <- roleStub
-          user <- userStub
-          roleWithId <- client.roles.create(role)
-          objWithId <- service(client).create(obj)
-          userWithId <- client.users.create(user)
-          isIdempotent <- roleService(client).roles.users.delete(objWithId.id, userWithId.id, roleWithId.id).valueShouldIdempotentlyBe(())
-        } yield isIdempotent
-      }
-      //#endregion
-    }
+  s"The ${name} service should handle role assignment" should {
+    scopedClient.map { client =>
+      val service = roleService(client)
+      behave like test(groupStub, client.groups, service.roles.groups)
+      behave like test(userStub, client.users, service.roles.users)
+    }.unsafeRunSync()
+  }
 }
