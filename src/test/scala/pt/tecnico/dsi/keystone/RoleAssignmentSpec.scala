8a593107caaf38bd4a9ef553aa610ae4d29108cd
Afonso Matos <afonsolfmatos@gmail.com>
2020-04-07 11:56:17 +0100

    Add more tests

--- src/test/scala/pt/tecnico/dsi/keystone/RoleAssignmentSpec.scala
+++ src/test/scala/pt/tecnico/dsi/keystone/RoleAssignmentSpec.scala
@@ -1,29 +1,54 @@
 package pt.tecnico.dsi.keystone
 
 import cats.effect.IO
+import org.scalatest.BeforeAndAfterEach
+
 import pt.tecnico.dsi.keystone.models.{Group, Role, User}
 import pt.tecnico.dsi.keystone.services.{CRUDService, RoleAssignmentService, RoleAssignment}
 
-trait RoleAssignmentSpec[T] { this: CRUDSpec[T] =>
+trait RoleAssignmentSpec[T] extends BeforeAndAfterEach { this: CRUDSpec[T] =>
+
+  var roles = 0
+  override def beforeEach = for {
+    client <- scopedClient
+    list <- service(client).list().compile.toList
+  } yield list.foreach(s => service(client).delete(s.id))
 
   def roleService: KeystoneClient[IO] => RoleAssignment[IO]
 
   def test[R](assignToStub: R, assignToCrud: CRUDService[IO, R], roleAssignmentService: RoleAssignmentService[IO]): Unit = {
-    def createStubs: IO[(String, String, String)] = for {
-      client <- scopedClient
-      objStub <- stub
-      obj <- service(client).create(objStub)
-      stub <- assignToCrud.create(assignToStub)
-      role <- client.roles.create(roleStub)
-    } yield (obj.id, stub.id, role.id)
+
+    def createStubs: IO[(String, String, String)] = {
+      roles += 1
+      for {
+        client <- scopedClient
+        objStub <- stub
+        obj <- service(client).create(objStub)
+        stub <- assignToCrud.create(assignToStub)
+        role <- client.roles.create(roleStub.copy(name = s"test-role#{$roles}"))
+      } yield (obj.id, stub.id, role.id)
+    }
 
     s"list roles for a ${assignToCrud.name} in a ${name}" in {
-      createStubs.flatMap { case (objId, stubId, _) =>
-        roleAssignmentService.list(objId, stubId).compile.toList.map(_.length).valueShouldIdempotentlyBe(0)
+      createStubs.flatMap { case (objId, stubId, roleId) =>
+        val roles = roleAssignmentService.list(objId, stubId).compile.toList
+        for {
+          _ <- roleAssignmentService.assign(objId, stubId, roleId)
+          idempotent <- roles.idempotently(_.exists(_.id == roleId) shouldBe true)
+        } yield idempotent
       }
     }
 
-    s"check ${assignToCrud.name} role assignment" in {
+    s"check ${assignToCrud.name} for role assignment" in {
+      createStubs.flatMap { case (objId, stubId, roleId) =>
+        for {
+          _ <- roleAssignmentService.assign(objId, stubId, roleId)
+          idempotent <- roleAssignmentService.check(objId, stubId, roleId).valueShouldIdempotentlyBe(true)
+        } yield idempotent
+      }
+    }
+
+    s"check ${assignToCrud.name} for no role assignment" in {
       createStubs.flatMap { case (objId, stubId, roleId) =>
         roleAssignmentService.check(objId, stubId, roleId).valueShouldIdempotentlyBe(false)
       }
@@ -38,6 +63,15 @@ trait RoleAssignmentSpec[T] { this: CRUDSpec[T] =>
     s"delete ${assignToCrud.name} role assignment" in {
       createStubs.flatMap { case (objId, stubId, roleId) =>
         roleAssignmentService.delete(objId, stubId, roleId).valueShouldIdempotentlyBe(())
+      }
+    }
+
+    s"check ${assignToCrud.name} role assignment after delete " in {
+      createStubs.flatMap { case (objId, stubId, roleId) =>
+        for {
+          _ <- roleAssignmentService.delete(objId, stubId, roleId)
+          check <- roleAssignmentService.check(objId, stubId, roleId).valueShouldBe(false)
+        } yield check
       }
     }
   }
